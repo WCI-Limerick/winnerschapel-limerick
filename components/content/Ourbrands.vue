c2a9a83f7949ddb5768525f9d51fac3af305198b
Olamide Toyinbo <toyinbo0903@outlook.com>
2026-08-25 19:23:16 +0100

    grid

--- components/content/Ourbrands.vue
+++ components/content/Ourbrands.vue
@@ -13,14 +13,14 @@
         :key="brand.alt"
         class="text-center w-40"
       >
-        <div class="h-65 md:h-73 lg:h-81 flex items-center justify-center mb-3">
+        <div class="h-62 md:h-70 lg:h-78 flex items-center justify-center mb-1">
           <img
             :src="brand.src"
             :alt="brand.alt"
             class="max-h-full max-w-full w-auto object-contain"
           >
         </div>
-        <p class="text-md md:text-base text-foreground">
+        <p class="text-sm md:text-base text-foreground">
           {{ brand.caption }}
         </p>
       </div>
