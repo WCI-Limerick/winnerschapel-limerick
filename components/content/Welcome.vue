40f47289663d53a78c28ccd4592b4a463c2f7301
Olamide Toyinbo <toyinbo0903@outlook.com>
2026-08-26 04:06:27 +0100

    svg

--- components/content/Welcome.vue
+++ components/content/Welcome.vue
@@ -2,7 +2,7 @@
   <section class="px-4 py-10 sm:px-6 lg:px-8 lg:py-14 border-t border-gray-200">
     <div class="flex flex-col sm:flex-row items-center sm:items-start gap-6 max-w-4xl mx-auto">
       <img
-        src="/Brown_Red_and_White_Bold_Communion_service_flyer_Instagram_Post_cropped_200w.webp"
+        src="/Brown_Red_and_White_Bold_Communion_service_flyer_Instagram_Post_cropped_200w.svg"
         alt="Youth Alive Limerick"
         class="w-36 h-36 md:w-44 md:h-44 shrink-0"
       >
